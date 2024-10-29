31a2f9e18f339f84d59559067c05e67e7130ffb9
RameenSheikh18 <rameensheikh493@gmail.com>
2024-10-29 06:32:31 -0700

    Add files via upload

---
+++ Assignment#1.py
@@ -0,0 +1,38 @@
+#variables 
+# Question no 1
+# write a program that calculate the sum,difference,product and average of three numbers?
+num3=30
+num2=20
+num3=40
+print("Sum of Three numbers=",num1+num2+num3) #Average of Three numbers=90
+print("Difference of Three numbers=",num1-num2-num3)#Difference of Three numbers= -40
+print("Product of Three numbers=",num1*num2*num3) #Product of Three numbers= 24000
+sum=num1+num2+num3
+print("Average of three numbers=",sum/3) #Average of three numbers=30.0
+
+#Question no 2
+# Write python code to calculate the area of the following shapes?
+# area=21*base*height , area=side2 , Area=π×radius2 , Area=length×width
+
+#Area=0.5×base×height
+base=8
+height=12
+area=0.5*base*height
+print("Area of triangle=",area) #Area of triangle=48.0
+
+#Area=side2 
+side=8
+area=side*side
+print("Area of square=",area) #Area of Square=64
+
+#Area=π×radius2
+r=6
+r=8
+area=4.132*r*r
+print("Area of circle=",area) #Area of circle=264.448
+
+#Area=length×width
+length=14
+width=18
+area=length*width
+print("Area of rectangle=",area) #Area of rectangle= 252
